661b2e9557d7e963e865e85dc2f15307bf6d49df
Rick Greaves <greavr@gmail.com>
2024-08-20 11:30:25 -0700

    Update setup.sh
    Adding in Docker and fixing terraform

--- setup.sh
+++ setup.sh
@@ -16,12 +16,11 @@ sudo apt-get update && sudo apt-get install -y google-cloud-cli kubectl google-c
 echo "Dont forget to run gcloud init"
 
 ## Installing Terraform
-sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-sudo apt update
-sudo apt-get install -y terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+terraform -install-autocomplete
 
 ## Install VSCode
 wget -O code-latest.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
@@ -32,5 +31,27 @@ rm code-latest.deb
 sudo apt-get install -y python3 python3-pip
 
 ## Setup dev
+git config --global user.name "Rick G"
+git config --global user.email "a@b.com"
 mkdir ~/code
 echo -e "\n\n\n" ssh-keygen -b 4096
+
+## Configure Docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## Install PodMon
+# sudo apt-get -y install podman
