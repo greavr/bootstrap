601e479025433e7178c1fb533989e9accdd59f06
Rick Greaves <greavr@gmail.com>
2025-05-29 23:11:26 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,8 +16,10 @@ sudo apt-get update && sudo apt-get install -y google-cloud-cli kubectl google-c
 
 ## Installing Terraform
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install terraform
 terraform -install-autocomplete
 
