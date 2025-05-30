24051ad7b5d5b5a35c54f28ffec1bf77b17dcebf
Rick Greaves <greavr@gmail.com>
2025-05-29 22:37:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 ## Install base packages
-sudo apt-get install --assume-yes -y cmatrix git-all wget curl htop nano gpg zsh cowsay fortune sl telnet tcpdump wireshark dnsutils
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmatrix git-all wget curl htop nano gpg zsh cowsay fortune sl telnet tcpdump wireshark dnsutils
 
 ## InstalL GCloud
 sudo apt-get install -y apt-transport-https ca-certificates gnupg curl
