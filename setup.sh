6037b5cb66adcd7f51914a1c6d9b8621f71126a9
Rick Greaves <greavr@gmail.com>
2025-06-10 09:37:23 -0700

    Update setup.sh
    Adding Helm support

--- setup.sh
+++ setup.sh
@@ -63,6 +63,12 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
 ## Install PodMon
 # sudo apt-get -y install podman
 
